97be9dd247aeccdf9908bfbe64a2c0fc120a1772
Deeptesh chaudhari <92786069+DeepteshChaudhari@users.noreply.github.com>
2022-09-15 15:13:13 +0530

    Add files via upload

---
+++ day-3-task-2.js
@@ -0,0 +1,26 @@
+let person=
+    {
+    name:"Deeptesh Chaudhari",
+    email:"diptsh151097@gmail.com",
+    phone:"8181031212",
+    degree:"undergraduate",
+  location:
+    {
+    address:"tathastu society,behind swaminarayan temple pune",
+    postalCode:"411001",
+    city:"pune"
+    },
+education:
+    {
+    "institution": "K.C.E's collage of eng. jalgaon",
+    "course": "electronics and telecommunication"
+    },
+languages: 
+    {
+    "language": "English, Hindi, Marathi",
+    "fluency": "excellent"
+      },
+    interests:
+      {
+        "name": "coding,chess,books"
+      }}
